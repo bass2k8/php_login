177eda775830f0dd08ba425f907e5294c663697c
Benjamin Staker <bassatcollege@googlemail.com>
2012-10-21 15:15:48 +0100

    Implemented logging output by request

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -6,9 +6,12 @@ Class Database {
 
 	private $_dbName; // Name of the Database.
 	private $_connection; // Connection object.
+	private $_logging=false; // Define whether to output logging or not.
+	private $_log = array(); // Log Array.
 
-	public function __construct($dbName){
+	public function __construct($dbName, $logging){
 		$this->_dbName = $dbName;
+		$this->_logging=$logging;
 		$this->_connect();
 		$this->_selectDatabase();
 	}
@@ -16,18 +19,34 @@ Class Database {
 	// Close the connection to the Database Server.
 	public function __destruct(){
 		mysql_close($this->_connection);
+		if($this->_logging){
+			$this->_log("Closed connection to ".DB_SERVER." successfully.");
+			$this->outputLog();
+		}
+	}
+
+	private function _log($text){
+		array_push($this->_log, $text);
+	}
+
+	public function outputLog(){
+		echo "<ul id=\"database_log\">\n";
+		foreach ($this->_log as $log) {
+			echo "<li>".$log."</li>\n";
+		}
+		echo "</ul>\n";
 	}
 
 	// Connecting to the Database Server.
 	private function _connect(){
 		$this->_connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS) or die("Could not connect to server: ".mysql_error());
-		//echo "Connected to ".DB_SERVER." successfully.\n";
+		if($this->_logging) $this->_log("Connected to ".DB_SERVER." successfully.");
 	}
 
 	// Selecting the Database.
 	private function _selectDatabase(){
 		mysql_select_db($this->_dbName, $this->_connection) or die("Could not select db: ".mysql_error());
-		//echo "Selected ".DB_DATABASE." successfully.\n";
+		if($this->_logging) $this->_log("Selected ".DB_DATABASE." successfully.");
 	}
 
 	// Querying the Database.
