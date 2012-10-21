c4bac82a5f7db2dc81b13f7a7f7dfb3ac64809ca
Benjamin Staker <bassatcollege@googlemail.com>
2012-10-21 17:43:18 +0100

    Implemented login and logout functions

--- inc/login.inc.php
+++ inc/login.inc.php
@@ -1,18 +1,45 @@
 <?php
 
 session_start();
-include("db.inc.php");
+require_once("log.inc.php");
+require_once("db.inc.php");
 
 Class Login {
 
-	private $_db; // Database object.
+	private $_log; // Log object.
+	private $_logging=false; // Define whether to output logging or not.
 
-	public function __construct(){
+	private $_db; // Database object.
+	private $_uid; // User ID.
+
+	public function __construct($logging){
+		$this->_logging=$logging;
+		if($this->_logging) $this->_log = new Log(get_class($this));
+
 		$this->_initDB();
+		if($this->loginStatus()) $this->_uid = $_SESSION["uid"];
 	}
 
 	private function _initDB(){
 		$this->_db = new Database("bass2k8_login", true);
+	}
+
+	public function loginStatus(){
+		if($_SESSION["uid"] && !empty($_SESSION["uid"])) return true;
+		else return false;
+	}
+
+	public function login($uid){
+		unset($_SESSION["uid"]);
+		$_SESSION["uid"] = $uid;
+		$this->_uid = $uid;
+		if($this->_logging) $this->_log->addToLog("The User with ID <strong>".$this->_uid."</strong> has successfully logged in.");
+	}
+
+	public function logout(){
+		unset($_SESSION["uid"]);
+		if($this->_logging) $this->_log->addToLog("The User with ID <strong>".$this->_uid."</strong> has successfully logged out.");
+		unset($this->_uid);
 	}
 
 }
