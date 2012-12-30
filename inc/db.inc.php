3dc31b41f50e2140b585949dc074385b4fbfff98
Benjamin Staker <bassatcollege@gmail.com>
2012-12-30 23:20:56 +0000

    Changed to PDO
    Updated the code to use PHP Data Objects (PDO), instead of the old
    (depreciated) mysql libraries.

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -9,40 +9,44 @@ Class Database {
 	private $_logging=false; // Define whether to output logging or not.
 
 	private $_dbName; // Name of the Database.
-	private $_connection; // Connection object.
+	private $_query; // SQL Query.
+	private $_db; // PDO database object.
 
 	public function __construct($dbName, $logging){
 		$this->_dbName = $dbName;
-		
+		$this->_query=false;
+
 		$this->_logging=$logging;
 		if($this->_logging) $this->_log = new Log(get_class($this));
 
 		$this->_connect();
-		$this->_selectDatabase();
 	}
 
 	// Close the connection to the Database Server.
 	public function __destruct(){
-		mysql_close($this->_connection);
-		if($this->_logging) $this->_log->addToLog("Closed connection to ".DB_SERVER." successfully.");
+		if($this->_query) $this->_query->closeCursor();
+		$this->_db=null;
+		if($this->_logging) $this->_log->addToLog("Closed connection to <strong>".DB_SERVER."</strong> successfully.");
 	}
 
 	// Connecting to the Database Server.
 	private function _connect(){
-		$this->_connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS) or die("Could not connect to server: ".mysql_error());
-		if($this->_logging) $this->_log->addToLog("Connected to <strong>".DB_SERVER."</strong> successfully.");
-	}
-
-	// Selecting the Database.
-	private function _selectDatabase(){
-		mysql_select_db($this->_dbName, $this->_connection) or die("Could not select db: ".mysql_error());
-		if($this->_logging) $this->_log->addToLog("Selected <strong>".$this->_dbName."</strong> successfully.");
+		$this->_db = new PDO("mysql:host=".DB_SERVER.";dbname=".$this->_dbName, DB_USER, DB_PASS);
+		if($this->_logging) $this->_log->addToLog("Connected to <strong>".DB_SERVER."</strong> and selected the database <strong>".$this->_dbName."</strong> successfully.");
 	}
 
 	// Querying the Database.
 	public function query($sql){
-		mysql_query($sql, $this->_connection) or die("Could not select db: ".mysql_error());
+		$this->_query = $this->_db->query($sql);
 		if($this->_logging) $this->_log->addToLog("Queried <strong>".$sql."</strong> successfully.");
+	}
+
+	// Fetch association.
+	public function fetchAssociation(){
+		if($this->_query){
+			if($this->_logging) $this->_log->addToLog("Fetched the association successfully.");
+			return $this->_query->fetch(PDO::FETCH_ASSOC);
+		}
 	}
 
 	// Select a Table.
--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
 
 require_once("inc/db.inc.php");
+
 $db = new Database("php_login", true);
+$db->query("SELECT * FROM personal");
+
+while($row = $db->fetchAssociation()) {
+	print_r($row);
+}
 
 echo "Hello World";
 
