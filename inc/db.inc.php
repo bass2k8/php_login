20c6b5c883d3646024fe5653f3ff68d8dba3a8aa
Benjamin Staker <bassatcollege@gmail.com>
2013-01-01 14:21:44 +0000

    Comments, Error Checking, New Method.
    Added comments for easier understanding of methods.
    Made PDO throw exceptions when an error has occurred, allowing the class
    to catch the errors.
    Every method now either returns true or false.
    New method: _PDOErrors().
    Made selecting table a protected method.

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -13,13 +13,15 @@ Class Database {
 	private $_db; // PDO database object.
 
 	public function __construct($dbName, $logging){
+		// Initialising the variables.
 		$this->_dbName = $dbName;
 		$this->_query=false;
-
 		$this->_logging=$logging;
+
+		// Create a log object, if logging is enabled.
 		if($this->_logging) $this->_log = new Log(get_class($this));
 
-		$this->_connect();
+		$this->_connect(); // Connect to the database.
 	}
 
 	// Close the connection to the Database Server.
@@ -31,14 +33,49 @@ Class Database {
 
 	// Connecting to the Database Server.
 	private function _connect(){
+		// Create a PDO object.
 		$this->_db = new PDO("mysql:host=".DB_SERVER.";dbname=".$this->_dbName, DB_USER, DB_PASS);
-		if($this->_logging) $this->_log->addToLog("Connected to <strong>".DB_SERVER."</strong> and selected the database <strong>".$this->_dbName."</strong> successfully.");
+		$this->_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode.
+		if(!$this->_PDOErrors()){
+			if($this->_logging) $this->_log->addToLog("Connected to <strong>".DB_SERVER."</strong> successfully.");
+			if($this->_logging) $this->_log->addToLog("Selected the database <strong>".$this->_dbName."</strong> successfully.");
+			return true;
+		} else {
+			if($this->_logging) $this->_log->addToLog("Could not connect to the Database server.");
+			return false;
+		}
+		
+	}
+	
+	// Check for any errors with PDO.
+	private function _PDOErrors(){
+		$error=$this->_db->errorInfo();
+		if(!$error[2]) return false;
+		else return true;
 	}
 
 	// Querying the Database.
 	public function query($sql){
-		$this->_query = $this->_db->query($sql);
-		if($this->_logging) $this->_log->addToLog("Queried <strong>".$sql."</strong> successfully.");
+		$error_status=false;
+
+		// If there already is a query, close it.
+		if($this->_query) $this->_query->closeCursor();
+		
+		try {
+			// Attempt to query the SQL.
+			$this->_query = $this->_db->query($sql);
+		} catch(PDOException $e){
+			// If an error was caught, add error to log.
+			$error_status=true;
+			if($this->_logging) $this->_log->addToLog("<strong>Error:</strong> ".$e->getMessage());
+			return false;
+		}
+
+		if(!$error_status){
+			// If there were no errors.
+			if($this->_logging) $this->_log->addToLog("Queried <strong>".$sql."</strong> successfully.");
+			return true;
+		}
 	}
 
 	// Fetch association.
@@ -46,14 +83,22 @@ Class Database {
 		if($this->_query){
 			if($this->_logging) $this->_log->addToLog("Fetched the association successfully.");
 			return $this->_query->fetch(PDO::FETCH_ASSOC);
+		} else {
+			if($this->_logging) $this->_log->addToLog("Cannot fetch association; there is no query.");
+			return false;
 		}
 	}
 
 	// Select a Table.
-	public function selectTable($table, $parameters){
-		if(!$parameters) $this->query("SELECT * FROM ".$table);
-		else $this->query("SELECT * FROM ".$table." ".$parameters);
-		if($this->_logging) $this->_log->addToLog("Selected <strong>".$table."</strong> successfully.");
+	protected function selectTable($table){
+		$this->query("SELECT * FROM `$table`");
+		if(!$this->_PDOErrors()){
+			if($this->_logging) $this->_log->addToLog("Selected <strong>".$table."</strong> successfully.");
+			return true;
+		} else {
+			if($this->_logging) $this->_log->addToLog("Could not select <strong>".$table."</strong>.");
+			return false;
+		}
 	}
 
 }
--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 require_once("inc/db.inc.php");
 
 $db = new Database("php_login", true);
-$db->query("SELECT * FROM personal");
 
 while($row = $db->fetchAssociation()) {
 	print_r($row);
